7516ad3726a6934f6bc565d5445d371b5eb4dd3d
izumi-t-yamana <t-yamana@izmc.co.jp>
2025-06-26 09:57:11 +0000

    docs

--- src/jjjexperiment/carryover_heat/section4_2.py
+++ src/jjjexperiment/carryover_heat/section4_2.py
@@ -172,6 +172,8 @@ def get_Theta_HBR_i_2023(
     # 熱容量(居室) [J/K]
     cbri = jjj_carryover_heat.get_C_BR_i(A_HCZ_i)
 
+    # TODO: 250625 梅本様よりコメントで絶対値としないのが正しいとのこと
+
     carryover_theta_diff = np.abs(Theta_HBR_before_i - Theta_star_HBR)
     # 熱繰越による熱容量[J] (1/1/0:00 なしとする)
     carryover_capacity = 0 if isFirst else cbri * carryover_theta_diff
